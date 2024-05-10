0f9688e3bd30d997fcaf2110d15c3ff269295e8c
andr3qa <andr3qaa@gmail.com>
2024-05-10 13:19:49 +0700

    Finished the header

--- gulp/tasks/html-include.js
+++ gulp/tasks/html-include.js
@@ -1,31 +1,39 @@
-import browserSync from 'browser-sync';
+import browserSync from "browser-sync";
 import fileInclude from "gulp-file-include";
 import typograf from "gulp-typograf";
-import bemValidator from 'gulp-html-bem-validator';
-import plumber from 'gulp-plumber';
-import notify from 'gulp-notify';
-import {
-  htmlValidator
-} from 'gulp-w3c-html-validator';
+import bemValidator from "gulp-html-bem-validator";
+import plumber from "gulp-plumber";
+import notify from "gulp-notify";
+import { htmlValidator } from "gulp-w3c-html-validator";
 
 export const htmlInclude = () => {
-  return app.gulp.src([`${app.paths.base.src}/*.html`])
-    .pipe(plumber(
-      notify.onError({
-        title: "HTML",
-        message: "Error: <%= error.message %>"
-      })))
-    .pipe(fileInclude({
-      prefix: '@',
-      basepath: '@file',
-      maxRecursion: 100
-    }))
-    .pipe(typograf({
-      locale: ['ru', 'en-US']
-    }))
-    .pipe(bemValidator())
-    .pipe(htmlValidator.analyzer())
-    .pipe(htmlValidator.reporter())
-    .pipe(app.gulp.dest(app.paths.base.build))
-    .pipe(browserSync.stream());
-}
+  return (
+    app.gulp
+      .src([`${app.paths.base.src}/*.html`])
+      .pipe(
+        plumber(
+          notify.onError({
+            title: "HTML",
+            message: "Error: <%= error.message %>",
+          })
+        )
+      )
+      .pipe(
+        fileInclude({
+          prefix: "@",
+          basepath: "@file",
+          maxRecursion: 100,
+        })
+      )
+      // .pipe(
+      //   typograf({
+      //     locale: ["ru", "en-US"],
+      //   })
+      // )
+      .pipe(bemValidator())
+      // .pipe(htmlValidator.analyzer())
+      // .pipe(htmlValidator.reporter())
+      .pipe(app.gulp.dest(app.paths.base.build))
+      .pipe(browserSync.stream())
+  );
+};
